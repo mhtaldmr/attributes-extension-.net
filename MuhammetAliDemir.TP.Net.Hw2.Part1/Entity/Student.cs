7ebe669a798dd71c3df32ec962023662ec68c1a1
mhtaldmr <mht.alidemir@gmail.com>
2022-05-26 13:23:26 +0300

    Adding the attributes.

--- MuhammetAliDemir.TP.Net.Hw2.Part1/Entity/Student.cs
+++ MuhammetAliDemir.TP.Net.Hw2.Part1/Entity/Student.cs
@@ -1,8 +1,13 @@
-﻿namespace MuhammetAliDemir.TP.Net.Hw2.Part1.Entity
+﻿using MuhammetAliDemir.TP.Net.Hw2.Part1.CustomAttributes.Attributes;
+
+namespace MuhammetAliDemir.TP.Net.Hw2.Part1.Entity
 {
+    [TableIdentifier("Student")]
     public class Student
     {
+        [ColumnIdentifier("Id", "int", isRequired: true)]
         public int Id { get; set; }
+        [ColumnIdentifier("Name", "string", isRequired: false)]
         public string Name { get; set; }
     }
 }
