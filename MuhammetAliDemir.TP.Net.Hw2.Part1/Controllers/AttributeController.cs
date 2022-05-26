0b08fdf4f4869c34e19cc82155c6b926aa4d1c0d
mhtaldmr <mht.alidemir@gmail.com>
2022-05-26 13:21:14 +0300

    AttributeController Added.

---
+++ MuhammetAliDemir.TP.Net.Hw2.Part1/Controllers/AttributeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MuhammetAliDemir.TP.Net.Hw2.Part1.Attributes;
+using MuhammetAliDemir.TP.Net.Hw2.Part1.Entity;
+
+namespace MuhammetAliDemir.TP.Net.Hw2.Part1.Controllers
+{
+    [Route("/attributes")]
+    [ApiController]
+    public class AttributeController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAttributes()
+        {
+            //Creating a new script which is coming from attributes
+            var result = ScriptGenerator.Generate(new Student());
+
+            Console.WriteLine(result);
+
+            return Ok(result);
+        }
+    }
+}
