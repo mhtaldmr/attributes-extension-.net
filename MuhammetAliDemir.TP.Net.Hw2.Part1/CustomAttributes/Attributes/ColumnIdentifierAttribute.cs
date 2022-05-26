2cd4d10bdd935910d69acd21c62652cadf1f797e
mhtaldmr <mht.alidemir@gmail.com>
2022-05-26 13:18:27 +0300

    CustomAttributes Created.

---
+++ MuhammetAliDemir.TP.Net.Hw2.Part1/CustomAttributes/Attributes/ColumnIdentifierAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace MuhammetAliDemir.TP.Net.Hw2.Part1.CustomAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnIdentifierAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsRequired { get; set; }
+
+        public ColumnIdentifierAttribute(string name, string type, bool isRequired)
+        {
+            Name = name;
+            Type = type;
+            IsRequired = isRequired;
+        }
+    }
+}
---
+++ MuhammetAliDemir.TP.Net.Hw2.Part1/CustomAttributes/Attributes/TableIdentifierAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace MuhammetAliDemir.TP.Net.Hw2.Part1.CustomAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableIdentifierAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public TableIdentifierAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
